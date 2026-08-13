29cb970dcffae69fd85ff6f3c41fc50fd58184aa
tristan-gscn <tristan.gscn@proton.me>
2026-08-13 23:37:11 +0200

    feat(ft_atoi_base): add parsing rules and base validation

--- srcs/ft_atoi_base_bonus.s
+++ srcs/ft_atoi_base_bonus.s
@@ -3,41 +3,89 @@ SECTION .note.GNU-stack noalloc noexec nowrite progbits
 SECTION .text
     GLOBAL ft_atoi_base
 
-EXTERN ft_strlen
-
 ft_atoi_base:
-    ; RCX final result
-    XOR RCX, RCX
+    XCHG RDI, RSI
 
-    ; RDX base length
-    PUSH RDI
-    MOV RDI, RSI
-    CALL ft_strlen
+    XOR R9D, R9D
+    CALL .char_val
     MOV RDX, RAX
-    POP RSI
 
-.loop:
-    CMP BYTE [RSI], 0
+    CMP RDX, 2
+    JB .invalid
+    XOR RCX, RCX
+.check_base:
+    CMP RCX, RDX
+    JE .skip_spaces
+    MOVZX R9D, BYTE [RDI + RCX]
+    CMP R9B, '+'
+    JE .invalid
+    CMP R9B, '-'
+    JE .invalid
+    CALL .is_space
+    JC .invalid
+    CALL .char_val
+    CMP RAX, RCX
+    JNE .invalid
+    INC RCX
+    JMP .check_base
+
+.skip_spaces:
+    MOVZX R9D, BYTE [RSI]
+    CALL .is_space
+    JNC .read_sign
+    INC RSI
+    JMP .skip_spaces
+
+.read_sign:
+    MOV R10, 1
+    CMP R9B, '+'
+    JE .skip_sign
+    CMP R9B, '-'
+    JNE .parse
+    NEG R10
+.skip_sign:
+    INC RSI
+
+.parse:
+    XOR RCX, RCX
+.parse_loop:
+    MOVZX R9D, BYTE [RSI]
+    CALL .char_val
+    CMP RAX, RDX
     JE .end
     IMUL RCX, RDX
-    CALL .char_val
     ADD RCX, RAX
     INC RSI
-    JMP .loop
+    JMP .parse_loop
 
 .end:
+    IMUL RCX, R10
     MOV RAX, RCX
+    RET
+
+.invalid:
+    XOR EAX, EAX
     RET
 
 .char_val:
     XOR RAX, RAX
-    MOVZX R9D, BYTE [RSI]
-    MOV R8, RDI
 .char_val_loop:
-    CMP R9B, BYTE [R8]
+    CMP BYTE [RDI + RAX], 0
+    JE .char_val_cut
+    CMP R9B, BYTE [RDI + RAX]
     JE .char_val_cut
     INC RAX
-    INC R8
     JMP .char_val_loop
 .char_val_cut:
+    RET
+
+.is_space:
+    CMP R9B, ' '
+    JE .is_space_yes
+    MOV R11B, R9B
+    SUB R11B, 9
+    CMP R11B, 5
+    RET
+.is_space_yes:
+    STC
     RET
